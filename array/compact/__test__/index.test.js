c7b88753945d42e58069eec35e521c27064ec25e
cuiyiming1998 <1269567664@qq.com>
2022-12-23 17:24:01 +0800

    ✨ feat: 添加array -- compact

---
+++ array/compact/__test__/index.test.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { compact } from '../index.js'
+
+describe('compact', () => {
+	const array = [0, '', null, 3, undefined, 5, false, NaN]
+	it('happy path', () => {
+		const arr = compact(array)
+		expect(arr).toStrictEqual([3, 5])
+	})
+})
---
+++ array/compact/index.js
@@ -0,0 +1,20 @@
+/**
+ * description
+ * 创建一个新数组, 包含原数组中所有的非假值元素。
+ * 例如false, null, 0, "", undefined, 和 NaN 都是被认为是“假值”。
+ *
+ * @param {Array} array 需要过滤的数组
+ * @return {Array} 过滤后的新array
+ *
+ * @example
+ * compact([0, '', null, 3, undefined, 5, false])
+ * return => [[3, 5]]
+ *
+ */
+export const compact = array => {
+	if (!Array.isArray(array)) {
+		return []
+	}
+  // 强转一下 如果不是false就不是假值
+	return array.filter(item => !!item)
+}
