c08a943fa3a64160daa2f605fb5caee6bae9e937
cuiyiming1998 <1269567664@qq.com>
2022-12-26 10:34:31 +0800

    💡 chore(difference): 添加difference注释

--- array/difference/index.js
+++ array/difference/index.js
@@ -1,5 +1,21 @@
 import { isArray } from '../../shared'
 
+/**
+ * description
+ * 创建一个具有唯一array值的数组，每个值不包含在其他给定的数组中。
+ * 注：即创建一个新数组，这个数组中的值，为第一个数字（array 参数）排除了给定数组中的值。
+ * 结果值的顺序是由第一个数组中的顺序确定。
+ *
+ * @param {Array} arr1 要检查的数组
+ * @param {Array} arr2 需要排除的值
+ * @return {Array} 过滤值后的新数组
+ *
+ * @example
+ * arr1 = [3, 2, 1]
+ * arr2 = [2, 4]
+ * return => [3, 1]
+ *
+ */
 export const difference = (arr1, arr2) => {
   if (!isArray(arr1, arr2)) {
     return []
