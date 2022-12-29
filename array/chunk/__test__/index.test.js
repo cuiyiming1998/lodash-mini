fb45955602167a2ed77ab91f493e77e1f6b5ca75
cuiyiming1998 <1269567664@qq.com>
2022-12-29 18:11:23 +0800

    :rocket: chore(*): 添加lint命令

--- array/chunk/__test__/index.test.js
+++ array/chunk/__test__/index.test.js
@@ -2,22 +2,25 @@ import { describe, it, expect } from 'vitest'
 import { chunk } from '../index.js'
 
 describe('chunk', () => {
-	const array = [0, 1, 2, 3, 4, 5]
-	it('happy path', () => {
-		const arr = chunk(array, 3)
-		expect(arr).toStrictEqual([
-			[0, 1, 2],
-			[3, 4, 5]
-		])
-	})
+  const array = [0, 1, 2, 3, 4, 5]
+  it('happy path', () => {
+    const arr = chunk(array, 3)
+    expect(arr).toStrictEqual([
+      [0, 1, 2],
+      [3, 4, 5]
+    ])
+  })
 
-it('should return [] when param is not array', () => {
+  it('should return [] when param is not array', () => {
     const arr = chunk(1, 3)
     expect(arr).toStrictEqual([])
   })
 
   it('should return the last chunk as remaining elements', () => {
-    const arr = chunk(array, 4);
-    expect(arr).toStrictEqual([[0, 1, 2, 3], [4, 5]]);
-  });
+    const arr = chunk(array, 4)
+    expect(arr).toStrictEqual([
+      [0, 1, 2, 3],
+      [4, 5]
+    ])
+  })
 })
